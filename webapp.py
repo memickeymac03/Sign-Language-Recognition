48bba23be7b6d760cf3b77110c055a51b137a45e
memickeymac03 <zubinbhaskar03@gmail.com>
2024-04-10 04:50:52 +0530

    Files ready for first upload on Github with readme file

---
+++ webapp.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, jsonify, Response
+from app import detect_gesture
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(detect_gesture(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
